8bf905ed6303856cc8b3b4ce81746457051634ac
DeveloperHuseyinAvci <developerhuseyinavci@hotmail.com>
2022-01-13 22:06:18 +0300

    coding-bootcamp-testimonial finished

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const textContent = document.querySelectorAll('.text-content')
+const imgProfile = document.querySelectorAll('.profile-img')
+
+const btnPrev = document.querySelector('.prev')
+const btnNext = document.querySelector('.next')
+
+let textContentNumber = textContent.length
+let imgProfileNumber = imgProfile.length
+let slideNo = 0
+
+
+btnPrev.addEventListener('click', prevFunc)
+btnNext.addEventListener('click', nextFunc)
+
+slideShow(slideNo)
+
+
+function nextFunc() {
+    slideNo++;
+    slideShow(slideNo)
+}
+
+function prevFunc() {
+    slideNo--;
+    slideShow(slideNo)
+}
+
+
+
+function slideShow(slideNumber) {
+    slideNo = slideNumber
+
+    if (slideNumber >= textContentNumber && slideNumber >= imgProfileNumber) slideNo = 0
+    if (slideNumber < 0) slideNo = slideNumber - 1
+
+    for (let i = 0; i < slideNumber; i++) {
+        textContent[i].style.display = "none"
+        imgProfile[i].style.display = "none"
+    }
+
+    textContent[slideNo].style.display = "block";
+    imgProfile[slideNo].style.display = "block";
+
+}
